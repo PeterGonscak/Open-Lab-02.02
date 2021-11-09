0bf9d7139b1f0656a3fba6ed98cd7f9ea7f0eccc
Peter Gonščák <peter.gonscak190@gmail.com>
2021-11-09 08:38:26 +0100

    done

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            string a = "";
+            foreach (var cislo in numbers)
+            {
+                string zaokruhlene = Math.Round(cislo, 2).ToString().Replace(',', '.');
+                a += zaokruhlene + " ";
+            }
+            string b = a.Remove(a.LastIndexOf(" "));
+            Console.Write(b);
         }
     }
 }
